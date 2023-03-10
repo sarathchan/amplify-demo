b510ecabd18edaf8336b9e5ac00ae9468b87edbf
sarathchan <sarathchan20@gmail.com>
2023-03-10 10:54:21 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import axios from "axios";
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import TextField from '@mui/material/TextField';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 function App() {
+  const [firstname, setFirstName] = useState("")
+  const [LastName, setLastName] = useState("")
+  const [Dob, setDob] = useState("")
+  const [Response, setResponse] = useState("")
+
+
+
+  const handleClick = () => {
+    const URL = 'https://xqqbxnn2kk4vhyqkrinahgyoa40swxwv.lambda-url.us-east-1.on.aws/'
+    let body = {
+      "question_type": firstname,
+      "question": LastName
+    }
+    let header = {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    }
+    console.log(body)
+    axios
+      .post(URL, JSON.stringify(body),header
+        )
+      .then((res) => {
+        console.log(res)
+        setResponse(res.data.response)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>
+        Chat Support
+      </h1>
+      <div className='input'>
+        <InputLabel id="demo-simple-select-helper-label">Question Type</InputLabel>
+
+        <Select
+        className='select'
+          labelId="demo-simple-select-autowidth-label"
+          id="demo-simple-select-helper"
+          value={firstname}
+          onChange={(e) => setFirstName(e.target.value)}
+          autoWidth
+          label="Question Type"
+
         >
-          Learn React
-        </a>
-      </header>
+
+          <MenuItem value='salesforce-developer'>Ask to Salesforce Team</MenuItem>
+          <MenuItem value='aws'>Ask to AWS Digital team</MenuItem>
+          <MenuItem value='solution-arch'>Ask to Solution Architect</MenuItem>
+        </Select>
+      </div>
+      <div className='input'>
+
+        <TextField id="outlined-basic" label="Enter Question" placeholder='Enter Question'
+          onChange={(e) => setLastName(e.target.value)} />
+      </div>
+      <div className='input'>
+
+        {/* <TextField id="outlined-basic" 
+          placeholder='Select DOB'
+          type="date"
+          onChange={setDob} />
+          </div>
+          <div className='login-btn'> */}
+<div>
+
+        <Button onClick={handleClick} variant="contained">Search</Button>
+</div>
+        <div className='response'>
+
+        <p >
+          {Response}
+        </p>
+        </div>
+      </div>
+
+
+
+
     </div>
   );
 }
